7488b354ece655622450aac41e0719b8105e9b24
himanshuhx <himanshuhx1221.com>
2020-06-15 17:49:12 +0530

    Find friends part email and name part

--- app/src/main/java/com/first75494/chatappx/FindFriends.java
+++ app/src/main/java/com/first75494/chatappx/FindFriends.java
@@ -1,21 +1,35 @@
 package com.first75494.chatappx;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.appcompat.widget.Toolbar;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class FindFriends extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private RecyclerView FindFriendsRecyclerList;
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
 
         FindFriendsRecyclerList = (RecyclerView) findViewById(R.id.find_friends_Recycler_list);
         FindFriendsRecyclerList.setLayoutManager(new LinearLayoutManager(this));
@@ -25,6 +39,55 @@ public class FindFriends extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Find Friends");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        Query query = firebaseFirestore.collection("users");
+
+        FirestoreRecyclerOptions<Contacts> options =
+                new FirestoreRecyclerOptions.Builder<Contacts>()
+                .setQuery(query, Contacts.class)
+                .build();
+
+        FirestoreRecyclerAdapter<Contacts,FindFriendViewHolder > adapter =
+                new FirestoreRecyclerAdapter<Contacts, FindFriendViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull FindFriendViewHolder holder, int position, @NonNull Contacts model) {
+                          holder.userName.setText(model.getName());
+                          holder.userStatus.setText(model.getEmail());
+                          //for image left part
+                    }
+
+                    @NonNull
+                    @Override
+                    public FindFriendViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                        View  view= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.users_display_layout,viewGroup,false);
+                        FindFriendViewHolder viewHolder = new FindFriendViewHolder(view);
+                        return viewHolder;
+                    }
+                };
+
+        FindFriendsRecyclerList.setAdapter(adapter);
+
+        adapter.startListening();
+    }
+
+    public static class FindFriendViewHolder extends RecyclerView.ViewHolder{
+
+        TextView userName,userStatus;
+        CircleImageView profileImage;
+
+        public FindFriendViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.users_profile_name);
+            userStatus = itemView.findViewById(R.id.users_status);
+            profileImage = itemView.findViewById(R.id.users_profile_image);
+        }
     }
 }
 
